dc4c6cc58748a901514532a87163682fb78515b6
Amir Marmul <amiruddinmarmul@gmail.com>
2023-04-09 12:14:07 +0800

    Adding detection of integer palindromes

--- lib/amirmarmul_palindrome.rb
+++ lib/amirmarmul_palindrome.rb
@@ -2,16 +2,28 @@
 
 require_relative "amirmarmul_palindrome/version"
 
-class String
-  
+module AmirmarmulPalindrome
+
   # Returns true for a palindrome, false otherwise.
   def palindrome?
-    processed_content == processed_content.reverse
+    if processed_content.empty?
+      false
+    else 
+      processed_content == processed_content.reverse
+    end
   end
 
   private 
     # Returns content for palindrome testing.
     def processed_content
-      scan(/[a-z]/i).join.downcase
+      to_s.scan(/[a-z0-9]/i).join.downcase
     end
+end
+
+class String
+  include AmirmarmulPalindrome
+end
+
+class Integer 
+  include AmirmarmulPalindrome
 end
--- test/test_amirmarmul_palindrome.rb
+++ test/test_amirmarmul_palindrome.rb
@@ -19,4 +19,16 @@ class TestAmirmarmulPalindrome < Minitest::Test
   def test_palindrome_with_punctuation
     assert "Madam, I'm Adam.".palindrome?
   end
+
+  def test_integer_non_palindrome
+    refute 12345.palindrome?
+  end
+
+  def test_integer_non_palindrome
+    assert 12321.palindrome?
+  end
+
+  def test_empty_string_not_palindrome
+    refute "".palindrome?
+  end
 end
