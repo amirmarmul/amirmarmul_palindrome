e5bfd9503a6db008545770784028c194352bb96a
Amir Marmul <amiruddinmarmul@gmail.com>
2023-04-09 11:08:16 +0800

    Initialize repository

---
+++ lib/amirmarmul_palindrome.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "amirmarmul_palindrome/version"
+
+module AmirmarmulPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/test_amirmarmul_palindrome.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class TestAmirmarmulPalindrome < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::AmirmarmulPalindrome::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
